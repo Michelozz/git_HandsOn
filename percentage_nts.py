6b48691f35c05ebd78aa91a14d2b5f458b512257
Laura Escudero <lauraescudero@Lauras-MacBook-Pro.local>
2023-02-20 23:26:48 -0500

    Additional comments were added to percentage_nts.py to further explain the script

--- percentage_nts.py
+++ percentage_nts.py
@@ -13,10 +13,17 @@ if len(sys.argv) == 1:
 
 args = parser.parse_args()
 
-# Transform the input sequence into upper cases
+# Transform the input sequence into upper cases. This allows the script to work even if the 
+sequence introduced was in lower case (the first step will be to change it to upper case).
+
 args.seq = args.seq.upper()
 
 # From a RNA or DNA sequence, obtain the percentages of each nucleotide
+# First we introduce an accumulator set to 0 for each nucleotide. This will add +1 everytime 
+it counts the nucleotide specified.
+# Then we report whether the sequence is DNA or RNA
+# Finally we provide the percentage of each nucleotide
+ 
 nt_contents = { "A":0, "T":0, "C":0, "G":0, "U":0} # accumulator
 if re.search('^[ACGTU]+$', args.seq):
     if 'U' in args.seq and 'T' in args.seq:
